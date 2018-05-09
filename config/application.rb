1d369458bab6089d049e7ff75c86b9bb9b607977
mwatha <mwathabwanali@gmail.com>
2018-05-09 15:57:39 +0200

    adding gem 'jwt'

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module DemographicsDataExchange
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
+    #autoloads lib folder during production
+    config.eager_load_paths << Rails.root.join('lib')
+
+    #autoloads lib folder during development
+    config.autoload_paths << Rails.root.join('lib')
+
   end
 end
