dbfd9b9eb99cef60aae9e01578408beee5e2042b
mwatha <mwathabwanali@gmail.com>
2018-05-09 15:03:40 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+  namespace :api do
+		namespace :v1 do
+			resources :users
+		end
+	end
+
+
+  post 'auth/register', to: 'api/v1/users#register'
+  post 'auth/login', to: 'api/v1/users#login'
+
 end
