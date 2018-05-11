679d45f24fba18d94f47ec78d0a2cffe0097acb9
Pachawo Bisani <phbisan@gmail.com>
2018-05-11 11:05:24 +0200

    added couch db npid model, modified couchdb location npid model

--- app/models/couchdb_location_npid.rb
+++ app/models/couchdb_location_npid.rb
@@ -1,2 +1,9 @@
 class CouchdbLocationNpid < CouchRest::Model::Base
+	
+  property  :assigned,            TrueClass, default: false
+  property  :couchdb_location_id, String     
+  property  :npid,                String
+
+  timestamps!
+
 end
