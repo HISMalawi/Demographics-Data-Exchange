90b1f4d8718235b102128081a514502e6fddc7de
mangochiman <mangochiman@gmail.com>
2018-05-23 16:30:18 +0200

    Verifying token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-	before_action :authenticate_request, except: %i[index]
+	before_action :authenticate_request, except: %i[index, verify_token]
 	attr_reader :current_user
 	
 	include ExceptionHandler
