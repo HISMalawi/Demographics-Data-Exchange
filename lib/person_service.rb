45d129728cc43907ef464736afbff1f567b63ac8
mangochiman <mangochiman@gmail.com>
2018-06-01 15:07:57 +0200

    Rescue nil for nil keys

--- lib/person_service.rb
+++ lib/person_service.rb
@@ -113,18 +113,18 @@ module PersonService
     birthdate               = params[:birthdate]
     birthdate_estimated     = params[:birthdate_estimated]
 
-    occupation              = params[:attributes][:occupation]
-    cellphone_number        = params[:attributes][:cellphone_number]
-    current_district        = params[:attributes][:current_district]
-    current_ta              = params[:attributes][:current_traditional_authority]
-    current_village         = params[:attributes][:current_village]
+    occupation              = params[:attributes][:occupation] rescue nil
+    cellphone_number        = params[:attributes][:cellphone_number] rescue nil
+    current_district        = params[:attributes][:current_district] rescue nil
+    current_ta              = params[:attributes][:current_traditional_authority] rescue nil
+    current_village         = params[:attributes][:current_village] rescue nil
 
-    home_district           = params[:attributes][:home_district]
-    home_ta                 = params[:attributes][:home_traditional_authority]
-    home_village            = params[:attributes][:home_village]
+    home_district           = params[:attributes][:home_district] rescue nil
+    home_ta                 = params[:attributes][:home_traditional_authority] rescue nil
+    home_village            = params[:attributes][:home_village] rescue nil
 
-    art_number              = params[:identifiers][:art_number]
-    htn_number              = params[:identifiers][:htn_number]
+    art_number              = params[:identifiers][:art_number] rescue nil
+    htn_number              = params[:identifiers][:htn_number] rescue nil
     
     
     if !given_name.blank?
