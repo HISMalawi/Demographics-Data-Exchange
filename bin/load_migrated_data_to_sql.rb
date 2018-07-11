b7a354f32439b060ba4ca90e72c5881fc47994e6
Pachawo Bisani <phbisan@gmail.com>
2018-07-11 10:50:50 +0200

    Load migrated data script

---
+++ bin/load_migrated_data_to_sql.rb
@@ -0,0 +1,25 @@
+def start
+  i = 1
+  data = ''
+  File.open("#{Rails.root}/log/people.sql", 'r').each_line do |line|
+    puts "Processing record #{i}"
+    data += "(#{line}),"
+    if (i % 100_000).zero? || 2_332_908 == i
+      puts 'Loading data into MySQL'
+      begin
+        ActiveRecord::Base.connection.execute <<EOF
+      	  INSERT INTO people (person_id, couchdb_person_id, 
+          given_name,middle_name,family_name,gender,birthdate,
+          birthdate_estimated,died,deathdate,deathdate_estimated,
+          location_created_at, creator,created_at,updated_at) VALUES #{data};
+  EOF
+      rescue StandardError => e
+        `echo "#{e}" >> #{Rails.root}/log/dde_load_migrated_data_errors.log`
+      end
+      data.clear
+    end
+    i += 1
+  end
+end
+
+start
