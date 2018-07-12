4e9f5561bc09ea39089a8a5d89375005246d6160
Pachawo Bisani <phbisan@gmail.com>
2018-07-12 16:29:21 +0200

    Added script to update attributes

--- bin/update_attributes.rb
+++ bin/update_attributes.rb
@@ -24,9 +24,9 @@ def start
 	  couchdb_person_id = doc['person_id']
 
 	  ActiveRecord::Base.connection.execute <<EOF
-	    UPDATE person_attributes SET couchdb_attribute_id = couchdb_attribute_id
-	    WHERE couchdb_person_id = couchdb_person_id
-	    AND couchdb_person_attribute_type_id = couchdb_attribute_type_id
+	    UPDATE person_attributes SET couchdb_attribute_id = #{couchdb_attribute_id}
+	    WHERE couchdb_person_id = #{couchdb_person_id}
+	    AND couchdb_person_attribute_type_id = #{couchdb_attribute_type_id}
 	  EOF
 	  puts "Updating #{couchdb_person_id}"
 	end
