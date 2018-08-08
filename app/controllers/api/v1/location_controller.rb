c044fb581f95bece02f3479807e15f8c5578d92e
Walter Kaunda <walterkaunda@live.co.uk>
2018-08-08 09:26:40 +0200

    Updated all controller actions to return content-type: 'application/json'.
    A majority of the actions where returning content-type 'text/plain'
    instead of 'application/json'. This was breaking the Dashboard as
    it treats any response that's not declared to be JSON as an error.

--- app/controllers/api/v1/location_controller.rb
+++ app/controllers/api/v1/location_controller.rb
@@ -1,29 +1,23 @@
 class Api::V1::LocationController < ApplicationController
-    
   def find
     location = Location.where(location_id: params[:location_id]).or(Location.where(couchdb_location_id: params[:location_id]))
     location = location.blank? ? [] : location.first
-    render plain: location.to_json
+    render json: location
   end
-  
+
   def list_assigned_locations
-    render plain: LocationService.list_assigned_locations.to_json
+    render json: LocationService.list_assigned_locations
   end
-  
+
   def npids_assigned
-    
-    render plain: NpidService.npids_assigned(params).to_json
-    
+    render json: NpidService.npids_assigned(params)
   end
-  
+
   def total_allocated_npids
-    
-    render plain: NpidService.total_allocated_npids(params).to_json
-    
+    render json: NpidService.total_allocated_npids(params)
   end
 
   def get_locations
-    render plain: LocationService.get_locations(params).to_json
+    render json: LocationService.get_locations(params)
   end
-
 end
--- app/controllers/api/v1/npid_controller.rb
+++ app/controllers/api/v1/npid_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::NpidController < ApplicationController
-
   def assign_npids
     npids = NpidService.assign(params[:limit], current_user)
-    render plain: npids.to_json
+    render json: npids
   end
-
 end
