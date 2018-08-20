19a7565edf265c71dfa8ad3579b4b97dffb53a45
Pachawo Bisani <phbisan@gmail.com>
2018-08-20 04:03:22 +0200

    Increated token life span to 8 hours

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,7 +3,7 @@ class JsonWebToken
 # our secret key to encode our jwt
 
   class << self
-    def encode(payload, exp = 2.hours.from_now)
+    def encode(payload, exp = 8.hours.from_now)
       # set token expiration time 
       payload[:exp] = exp.to_i
       
