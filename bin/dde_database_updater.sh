94d7eb04bb99bf9b9f9811c424aae9ff4147048d
mwatha <mwathabwanali@gmail.com>
2018-09-07 15:50:21 +0200

    adding bash for update: 20%

--- bin/dde_database_updater.sh
+++ bin/dde_database_updater.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ENV=$1
 
+
+
+Updatecouchdbrole () {
+  key='_id';
+  value=$(echo $1 | sed 's/{.*\'$key'":"*\([0-9a-zA-Z|.|_|-|\/]*\)"*,*.*}/\1/');
+  echo $value
+}
+
 MYSQL_USERNAME=`ruby -ryaml -e "puts YAML::load_file('../config/database.yml')['${ENV}']['username']"`
 MYSQL_PASSWORD=`ruby -ryaml -e "puts YAML::load_file('../config/database.yml')['${ENV}']['password']"`
 MYSQL_DATABASE=`ruby -ryaml -e "puts YAML::load_file('../config/database.yml')['${ENV}']['database']"`
@@ -18,23 +26,41 @@ COUCH_DATABASE=${COUCH_PREFIX}_${COUCH_SUFFIX}
 # Check if last_sequence.txt file exists
 if [ ! -x ../log/last_sequence.txt ] ; then
     touch ../log/last_sequence.txt
-    echo "{'last_sequence' : '0'}" > ../log/last_sequence.txt
+    echo "last_seq: 0" > ../log/last_sequence.txt
 fi
 
-filename="../log/last_sequence.txt"
+LAST_SEQ=`ruby -ryaml -e "puts YAML::load_file('../log/last_sequence.txt')['last_seq']"`;
 
-while read -r line
-do 
-  LS="$line"
-done < "$filename"
+RESULTS=`curl "${COUCH_PROTOCOL}://${COUCH_HOST}:${COUCH_PORT}/${COUCH_DATABASE}/_changes?since=${LAST_SEQ}&limit=10000"`;
 
-LAST_SEQUENCE=`echo "$LS" | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $2}'`
-LAST_SEQUENCE=`echo ${LAST_SEQUENCE} | xargs`
-#LAST_SEQUENCE=sed -i "s/last_sequence/g" ../log/last_sequence.txt;
+RECORDS=`echo $RESULTS | awk '{split($0,a,"last_seq:"); print a[3]; print a[2]; print a[1]}'`;
 
-RESULTS=echo curl "${COUCH_PROTOCOL}://${COUCH_HOST}:${COUCH_PORT}/${COUCH_DATABASE}/_changes?since=${LAST_SEQUENCE}&limit=10000"
-LAST_SEQ=`echo "$RESULTS" | sed -e 's/"last_seq":/''/g' | awk -v RS=',' -F: '{print $1}'`
+LAST_SEQ=`echo $RECORDS | sed 's/.*,//'`;
+LAST_SEQ=`echo "${LAST_SEQ//\}}"`;
 
-#echo $LAST_SEQ
+LAST_SEQ_NUM=`echo $LAST_SEQ | tr -dc '0-9'`;
 
+echo "$RESULTS" > ../log/latest_coucdb_docs.txt
+RECORDS=`ruby -ryaml -e "puts YAML::load_file('../log/latest_coucdb_docs.txt')['results']"`;
+
+
+for i in $RECORDS
+do
+  if [[ $i = *"id"* ]] ; then
+    DOC_ID=`echo $i | awk '{split($0,a,"=>"); print a[2]}'`;
+    DOC_ID=`echo "${DOC_ID//,}"`;
+    DOC_ID=`echo "${DOC_ID//\"}"`;
+    DOC=`curl "${COUCH_PROTOCOL}://${COUCH_HOST}:${COUCH_PORT}/${COUCH_DATABASE}/${DOC_ID}"`;
+
+    if [[ $DOC = *"CouchdbRole"* ]] ; then
+      Updatecouchdbrole $DOC;
+    fi
+    exit;
+  fi
+done
+
+
+
+
+exit;
 
