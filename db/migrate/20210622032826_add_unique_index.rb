426b038a5cc30ca32a877d9929c426b0534ae78b
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-06-23 15:59:47 +0200

    Removed npid and person_uuid unique indexing

--- db/migrate/20210622032826_add_unique_index.rb
+++
@@ -1,6 +0,0 @@
-class AddUniqueIndex < ActiveRecord::Migration[5.2]
-  def change
-    add_index :person_details, :npid, unique: true
-    add_index :person_details, :person_uuid, unique: true
-  end
-end
