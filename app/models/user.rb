27b6510a977d305b829ebc54084148272a16eef2
Senzenjani <smanjawira@gmail.com>
2021-06-24 10:37:58 +0200

    Added Test to allow authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-   validates_presence_of :username, :password_digest
+   validates_presence_of :username, 
+   :password_digest
    validates :username, uniqueness: true
    mattr_accessor :current
    #encrypt password
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,10 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /show" do
-    it "works! (now write some real specs)" do
-      get api_v1_users_path
-      expect(response).to have_http_status(200)
+
+  def authenticated_header(user)
+    token = Knock::AuthToken.new(payload: { sub: user.id}).token
+    {'Authorization': "Bearer #{token}"}
+  end
+
+  describe '/v1/verify_token/' do
+    URL = '/v1/verify_token/'
+    UATH_URL = '/v1/verify_token/'
+  
+
+    context 'when the request with no authentication header' do
+      it 'should return unauth for retrieve current user info before login' do
+        post URL
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when the request contains an aunthentication header' do
+      it 'should return the user info' do
+        user = create(:user)
+
+        get URL, headers: authenticated_header(user)
+        puts response.body
+      end
     end
   end
+
+
+  describe "GET /show" do
+    it "request for all users" do
+      user = User.add_user(username: "TestUser",password_digest: "ghjgfdnnvn778")
+      get api_v1_users_path
+      expect(response).to be_successful
+      expect(response.body).to include("TestUser")
+    end
+  end 
 end
+
