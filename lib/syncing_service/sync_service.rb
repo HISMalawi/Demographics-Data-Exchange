e8a81c675722e9d86ab3fd42d8bab0d3b4c4122e
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-06-30 16:48:51 +0200

    Removed sync bug

--- lib/syncing_service/sync_service.rb
+++ lib/syncing_service/sync_service.rb
@@ -22,7 +22,7 @@ module SyncService
     push_seq = PushTracker.find_by(site_id: data[:location_updated_at].to_i, push_type: 'update')
 
     if push_seq.blank?
-      push_seq = PushTracker.create!(site_id: data[:location_updated_at].to_i,push_seq_update: 0, push_type: 'update')
+      push_seq = PushTracker.create!(site_id: data[:location_updated_at].to_i,push_seq: 0, push_type: 'update')
     end
 
     return {status: 200} if push_seq.push_seq > data[:id].to_i # Skip data if has already been tracked
@@ -45,7 +45,7 @@ module SyncService
             audit_record.delete('update_seq')
             PersonDetailsAudit.create!(audit_record)
         end
-        push_seq.update(push_seq_update: current_seq)
+        push_seq.update(push_seq: current_seq)
         return {status: 200, push_seq: current_seq}
       end
   end
@@ -55,7 +55,7 @@ module SyncService
     push_seq = PushTracker.find_by(site_id: data[:location_updated_at].to_i, push_type: 'new')
 
     if push_seq.blank?
-      push_seq = PushTracker.create!(site_id: data[:location_updated_at].to_i,push_seq_new: 0, push_type: 'new')
+      push_seq = PushTracker.create!(site_id: data[:location_updated_at].to_i,push_seq: 0, push_type: 'new')
     end
 
     return {status: 200} if push_seq.push_seq > data[:id].to_i # Skip data if has already been tracked
@@ -78,7 +78,7 @@ module SyncService
             audit_record.delete('update_seq')
             PersonDetailsAudit.create!(audit_record)
         end
-        push_seq.update(push_seq_new: current_seq)
+        push_seq.update(push_seq: current_seq)
         return {status: 200, push_seq: current_seq}
       end
   end
