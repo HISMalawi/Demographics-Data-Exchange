d265fa9d1b61d1289c483365a769f91c8828b7d2
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-07-07 06:05:04 +0200

    Paralized batch migration processing

--- bin/master_npid_updater.rb
+++ bin/master_npid_updater.rb
@@ -56,6 +56,7 @@ end
 
 def format_person(person,db)
   #attributes = get_address(person['person_id'],db)
+
   person = {
     id:                     'NULL',
     first_name:             "\"#{person['given_name']}\"",
@@ -72,8 +73,8 @@ def format_person(person,db)
     home_village:           "\"#{person['home_village']}\"",
     npid:                   "\"#{person['npid']}\"",
     person_uuid:            "\"#{person['couchdb_person_id']}\"",
-    date_registered:        "\"#{person['created_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"",
-    last_edited:            "\"#{person['updated_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"",
+    date_registered:        ("\"#{person['created_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"" rescue "\"1900-01-01 00:00:00\""),
+    last_edited:            ("\"#{person['updated_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"" rescue "\"1900-01-01 00:00:00\""),
     location_created_at:    person['location_created_at'],
     location_updated_at:    person['location_created_at'],
     created_at:             'now()',
@@ -88,20 +89,14 @@ def format_person(person,db)
   }
 end
 
-def batch_process(query)
-  offset = 0
-  begin
-    batch = ActiveRecord::Base.connection.select_all <<-SQL
-      #{query}
-      LIMIT #{@batch_size}
-      OFFSET #{offset}
-    SQL
-    batch.each do |row|
-      yield row
-    end
-    offset += @batch_size
-  end until batch.empty?
-end
+# def batch_process(query)
+#     batch = ActiveRecord::Base.connection.select_all <<-SQL
+#       #{query}
+#     SQL
+#     batch.each do |row|
+#       yield row
+#     end
+# end
 
 def import_non_duplicate_records(database)
   loaded_people = []
@@ -110,55 +105,71 @@ def import_non_duplicate_records(database)
     loaded_people << uuid['person_uuid']
     loaded_npids << uuid['npid']
   end
+  min = 0
+  max = @batch_size.dup
+  max_id = ActiveRecord::Base.connection.select_all("SELECT max(person_id) max_id FROM #{database}.people").first['max_id'].to_i
 
-  select_people = %Q(SELECT * FROM #{database}.people pple LEFT JOIN
-                    (
-                    SELECT anc.person_id, anc.ancestry_district, anc_ta.ancestry_ta, anc_village.ancestry_village,
-                    hme_district.home_district,
-                    home_ta.home_ta,
-                    hme_vllg.home_village
-                    FROM
-                    (SELECT pa.person_id, pa.value ancestry_district FROM #{database}.person_attributes pa
-                    WHERE pa.person_attribute_type_id = 4 and pa.voided = 0) anc
-                    JOIN
-                    (SELECT pa.person_id, pa.value ancestry_ta FROM #{database}.person_attributes pa
-                    WHERE pa.person_attribute_type_id = 5 and pa.voided = 0) anc_ta
-                    ON anc.person_id = anc_ta.person_id
-                    JOIN
-                    (SELECT pa.person_id, pa.value ancestry_village FROM #{database}.person_attributes pa
-                    WHERE pa.person_attribute_type_id = 6 and pa.voided = 0) anc_village
-                    ON anc.person_id = anc_village.person_id
-                    JOIN
-                    (SELECT pa.person_id, pa.value home_district FROM #{database}.person_attributes pa
-                    WHERE pa.person_attribute_type_id = 1 and pa.voided = 0) hme_district
-                    ON anc.person_id = hme_district.person_id
-                    JOIN
-                    (SELECT pa.person_id, pa.value home_ta FROM #{database}.person_attributes pa
-                    WHERE pa.person_attribute_type_id = 2 and pa.voided = 0) home_ta
-                    ON anc.person_id = home_ta.person_id
-                    JOIN
-                    (SELECT pa.person_id, pa.value home_village FROM #{database}.person_attributes pa
-                    WHERE pa.person_attribute_type_id = 3 and pa.voided = 0) hme_vllg
-                    ON anc.person_id = hme_vllg.person_id) attr
-                    ON pple.person_id = attr.person_id
-                    WHERE length(npid) > 0
-                    AND given_name is not null
-                    AND family_name is not null
-                    )
-  records = []
-  batch_process(select_people) do | person|
-     next if loaded_people.include?(person['couchdb_person_id']) || loaded_npids.include?(person['npid'])
-     records << format_person(person,database)
-     loaded_people << person['couchdb_person_id']
-     loaded_npids << person['npid']
-     #puts records.count
-     if (records.size % @batch_size == 0)
-        import_record(records)
-        records = []
-     end
+  n = ((max_id / @batch_size) + 1).to_i
+
+  puts "To process #{n} batches"
+
+  select_people = []
+  (1..n).each do |num|
+    select_people << %Q(SELECT * FROM #{database}.people pple LEFT JOIN
+                      (
+                      SELECT anc.person_id, anc.ancestry_district, anc_ta.ancestry_ta, anc_village.ancestry_village,
+                      hme_district.home_district,
+                      home_ta.home_ta,
+                      hme_vllg.home_village
+                      FROM
+                      (SELECT pa.person_id, pa.value ancestry_district FROM #{database}.person_attributes pa
+                      WHERE pa.person_attribute_type_id = 4 and pa.voided = 0) anc
+                      JOIN
+                      (SELECT pa.person_id, pa.value ancestry_ta FROM #{database}.person_attributes pa
+                      WHERE pa.person_attribute_type_id = 5 and pa.voided = 0) anc_ta
+                      ON anc.person_id = anc_ta.person_id
+                      JOIN
+                      (SELECT pa.person_id, pa.value ancestry_village FROM #{database}.person_attributes pa
+                      WHERE pa.person_attribute_type_id = 6 and pa.voided = 0) anc_village
+                      ON anc.person_id = anc_village.person_id
+                      JOIN
+                      (SELECT pa.person_id, pa.value home_district FROM #{database}.person_attributes pa
+                      WHERE pa.person_attribute_type_id = 1 and pa.voided = 0) hme_district
+                      ON anc.person_id = hme_district.person_id
+                      JOIN
+                      (SELECT pa.person_id, pa.value home_ta FROM #{database}.person_attributes pa
+                      WHERE pa.person_attribute_type_id = 2 and pa.voided = 0) home_ta
+                      ON anc.person_id = home_ta.person_id
+                      JOIN
+                      (SELECT pa.person_id, pa.value home_village FROM #{database}.person_attributes pa
+                      WHERE pa.person_attribute_type_id = 3 and pa.voided = 0) hme_vllg
+                      ON anc.person_id = hme_vllg.person_id) attr
+                      ON pple.person_id = attr.person_id
+                      WHERE length(npid) > 0
+                      AND given_name is not null
+                      AND family_name is not null
+                      AND pple.person_id > #{min}
+                      AND pple.person_id <= #{max}
+                      )
+     min += @batch_size.dup
+     max += @batch_size.dup
   end
-  import_record(records)
-  records = []
+
+  Parallel.each(select_people) do |query|
+    records = []
+    batch = ActiveRecord::Base.connection.select_all <<-SQL
+       #{query}
+     SQL
+    batch.each do | person|
+       next if loaded_people.include?(person['couchdb_person_id']) || loaded_npids.include?(person['npid'])
+       records << format_person(person,database)
+       loaded_people << person['couchdb_person_id']
+       loaded_npids << person['npid']
+    end
+    puts records.size
+    import_record(records) if records.size > 0
+    records = []
+  end
 end
 
 def import_record(people)
@@ -177,52 +188,52 @@ end
 
 
 def main
-    load_dumps
+    #load_dumps
 
     site_databases = ActiveRecord::Base.connection.execute <<~SQL
       SHOW DATABASES LIKE 'npid_update%';
     SQL
 
-    site_databases.each do | database |
-      update_npids = "UPDATE #{@database_main}.npids SET assigned = 1 WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids);"
+    # site_databases.each do | database |
+    #   update_npids = "UPDATE #{@database_main}.npids SET assigned = 1 WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids);"
 
-      ActiveRecord::Base.connection.execute(update_npids)
+    #   ActiveRecord::Base.connection.execute(update_npids)
 
-      unavailable_npids = "SELECT #{LocationNpid.column_names[1..7].join(', ')} FROM #{database[0]}.location_npids WHERE npid NOT IN (SELECT npid FROM #{@database_main}.location_npids) AND length(npid) <> 0;"
+    #   unavailable_npids = "SELECT #{LocationNpid.column_names[1..7].join(', ')} FROM #{database[0]}.location_npids WHERE npid NOT IN (SELECT npid FROM #{@database_main}.location_npids) AND length(npid) <> 0;"
 
-      un_npids = ActiveRecord::Base.connection.execute(unavailable_npids)
+    #   un_npids = ActiveRecord::Base.connection.execute(unavailable_npids)
 
-      sql_batch_insert = init_insert
+    #   sql_batch_insert = init_insert
 
-      count = un_npids.count
+    #   count = un_npids.count
 
-      un_npids.each_with_index do |row, n|
-        	row.each_with_index do | value, i |
-        	 sql_batch_insert << "(\"#{value}\"," if i == 0
-        	 sql_batch_insert << "\"#{value}\"," if i < 5 && i > 0
-        	 sql_batch_insert << ("\"#{value.to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"," rescue "\"#{row[6].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"," rescue '"1900-01-01 00:00:00",') if i == 5
-        	 sql_batch_insert << ("\"#{value.to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\")," rescue "\"#{row[5].strftime.to_datetime('%Y-%m-%d %H:%M:%S')}\")," rescue '"1900-01-01 00:00:00"),') if i == 6
-        	end
-        if n > 0
-      	  if (n + 1) % @batch_size == 0 || count == n + 1
-      	  	ActiveRecord::Base.connection.execute(sql_batch_insert.chomp!(','))
-      	  	sql_batch_insert = init_insert
-      	  end
-        end
-      end
-      puts "updating location_npids assigned state for #{database[0]}"
+    #   un_npids.each_with_index do |row, n|
+    #     	row.each_with_index do | value, i |
+    #     	 sql_batch_insert << "(\"#{value}\"," if i == 0
+    #     	 sql_batch_insert << "\"#{value}\"," if i < 5 && i > 0
+    #     	 sql_batch_insert << ("\"#{value.to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"," rescue "\"#{row[6].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"," rescue '"1900-01-01 00:00:00",') if i == 5
+    #     	 sql_batch_insert << ("\"#{value.to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\")," rescue "\"#{row[5].strftime.to_datetime('%Y-%m-%d %H:%M:%S')}\")," rescue '"1900-01-01 00:00:00"),') if i == 6
+    #     	end
+    #     if n > 0
+    #   	  if (n + 1) % @batch_size == 0 || count == n + 1
+    #   	  	ActiveRecord::Base.connection.execute(sql_batch_insert.chomp!(','))
+    #   	  	sql_batch_insert = init_insert
+    #   	  end
+    #     end
+    #   end
+    #   puts "updating location_npids assigned state for #{database[0]}"
 
-      update_location_npid_state = "UPDATE #{@database_main}.location_npids SET assigned = 1 WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids WHERE assigned = 1);"
+    #   update_location_npid_state = "UPDATE #{@database_main}.location_npids SET assigned = 1 WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids WHERE assigned = 1);"
 
-      ActiveRecord::Base.connection.execute(update_location_npid_state)
-    end
+    #   ActiveRecord::Base.connection.execute(update_location_npid_state)
+    # end
 
     #Migrate data
     site_databases.each do |database|
       begin
         import_non_duplicate_records(database[0])
       rescue => e
-        File.write("#{Rails.root}/log/migration_errors.log",e,mode: 'a' )
+         File.write("#{Rails.root}/log/migration_errors.log",e,mode: 'a' )
       end
     end
       # sql = "DROP database #{database[0]};"
