c117554d309442778621d36d6f6edf63c9fe01e2
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-08-29 03:30:15 +0200

    Considering all possiblity of npid assignment

--- bin/master_npid_updater.rb
+++ bin/master_npid_updater.rb
@@ -8,7 +8,7 @@ config   = Rails.configuration.database_configuration
 
 
 def init_insert
-	sql = "INSERT INTO `location_npids` (`couchdb_location_npid_id`, `couchdb_location_id`, `location_id`, `npid`, `assigned`,`created_at`, `updated_at`) VALUES "
+	sql = "INSERT INTO `location_npids` (`location_id`, `npid`, `assigned`,`created_at`, `updated_at`) VALUES "
 end
 
 def load_file(db, file)  
@@ -43,31 +43,38 @@ Parallel.each_with_index(files) do | file, i |
   puts "Loading #{file} into npid_update#{i}"
  `pv #{file} | gunzip | mysql -u#{@username} -p#{@password} npid_update#{i}`
 end
-
   site_databases = ActiveRecord::Base.connection.execute <<~SQL
     SHOW DATABASES LIKE 'npid_update%';
   SQL
 
-  site_databases.each do | database |
-    update_npids = "UPDATE #{@database_main}.npids SET assigned = 1 WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids);"
+  site_databases.each_with_index do | database, i |
+    puts "updating database #{i} / #{site_databases.count} named #{database[0]}"
+
+    ActiveRecord::Base.connection.execute("ALTER TABLE #{database[0]}.location_npids add index(npid)")
+
+    ActiveRecord::Base.connection.execute("ALTER TABLE #{database[0]}.people add index(npid)")
+
+    update_npids = "UPDATE #{@database_main}.npids SET assigned = true WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids);"
 
     ActiveRecord::Base.connection.execute(update_npids)
 
-    unavailable_npids = "SELECT #{LocationNpid.column_names[1..7].join(', ')} FROM #{database[0]}.location_npids WHERE npid NOT IN (SELECT npid FROM #{@database_main}.location_npids) AND length(npid) <> 0;"
+    update_npids = "UPDATE #{@database_main}.npids SET assigned = true WHERE npid IN (SELECT npid FROM #{database[0]}.people where npid is not null);"
+
+    ActiveRecord::Base.connection.execute(update_npids)
+
+    unavailable_npids = "SELECT * FROM #{database[0]}.location_npids WHERE npid NOT IN (SELECT npid FROM #{@database_main}.location_npids) AND length(npid) = 6;"
     
-    un_npids = ActiveRecord::Base.connection.execute(unavailable_npids)
-    
+    un_npids = ActiveRecord::Base.connection.exec_query(unavailable_npids)
     sql_batch_insert = init_insert
 
     count = un_npids.count
 
     un_npids.each_with_index do |row, n|
-      	row.each_with_index do | value, i |
-      	 sql_batch_insert << "(\"#{value}\"," if i == 0
-      	 sql_batch_insert << "\"#{value}\"," if i < 5 && i > 0
-      	 sql_batch_insert << ("\"#{value.to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"," rescue "\"#{row[6].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\"," rescue '"1900-01-01 00:00:00",') if i == 5
-      	 sql_batch_insert << ("\"#{value.to_datetime.strftime('%Y-%m-%d %H:%M:%S')}\")," rescue "\"#{row[5].strftime.to_datetime('%Y-%m-%d %H:%M:%S')}\")," rescue '"1900-01-01 00:00:00"),') if i == 6
-      	end
+         sql_batch_insert << '(' + row['location_id'].to_s + ','
+      	 sql_batch_insert <<  "'#{row['npid']}',"
+      	 sql_batch_insert << "#{row['assigned']},"
+      	 sql_batch_insert << ("'#{row['created_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}'," rescue "'#{row['updated_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}'," rescue '"1900-01-01 00:00:00",')
+      	 sql_batch_insert << ("'#{row['updated_at'].to_datetime.strftime('%Y-%m-%d %H:%M:%S')}')," rescue "'#{row['created_at'].strftime.to_datetime('%Y-%m-%d %H:%M:%S')}')," rescue '"1900-01-01 00:00:00"),')
       if n > 0
     	  if (n + 1) % @batch_size == 0 || count == n + 1 
     	  	ActiveRecord::Base.connection.execute(sql_batch_insert.chomp!(','))
@@ -77,13 +84,17 @@ end
     end
     puts "updating location_npids assigned state for #{database[0]}"
     
-    update_location_npid_state = "UPDATE #{@database_main}.location_npids SET assigned = 1 WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids WHERE assigned = 1);"
+    update_location_npid_state = "UPDATE #{@database_main}.location_npids SET assigned = true WHERE npid IN (SELECT npid FROM #{database[0]}.location_npids WHERE assigned = true);"
     
     ActiveRecord::Base.connection.execute(update_location_npid_state)
 
-    sql = "DROP database #{database[0]};"
-    puts "Cleaning #{database[0]}"
-    ActiveRecord::Base.connection.execute(sql)
+    update_location_npid_state = "UPDATE #{@database_main}.location_npids SET assigned = true WHERE npid IN (SELECT npid FROM #{database[0]}.people WHERE npid is not null);"
+
+    ActiveRecord::Base.connection.execute(update_location_npid_state)
+
+    # sql = "DROP database #{database[0]};"
+    # puts "Cleaning #{database[0]}"
+    # ActiveRecord::Base.connection.execute(sql)
   end
 
 
