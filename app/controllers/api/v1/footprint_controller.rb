c80cf45af8cf6738147b59c2fc7a1f93b7467b09
Fredrick Chiyenda <fchiyenda@gmail.com>
2021-09-08 09:31:42 +0200

    Adapted footprint to get person_uuid the way its send

--- app/controllers/api/v1/footprint_controller.rb
+++ app/controllers/api/v1/footprint_controller.rb
@@ -12,7 +12,8 @@ class Api::V1::FootprintController < ApplicationController
   private
    def foot_params
     params.require([:user_id,:person_uuid,:program_id,:location_id,:encounter_datetime])
-    {user_id: params[:user_id],person_uuid: params[:person_uuid],program_id: params[:program_id], location_id: params[:location_id],
+    params.permit(:user_id,:person_uuid,:program_id,:location_id,:encounter_datetime)
+    {user_id: params[:user_id],person_uuid: params[:person_uuid][:identifier],program_id: params[:program_id], location_id: params[:location_id],
       encounter_datetime: params[:encounter_datetime]}
    end
 
