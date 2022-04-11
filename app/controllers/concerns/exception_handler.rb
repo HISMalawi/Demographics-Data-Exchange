821cc7e9bc4326205a2fb62325a22a94bdd75ddf
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-11 14:09:26 +0200

    initial on status handling

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -24,6 +24,10 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       render json: { message: e.message }, status: :unprocessable_entity
     end
+
+    rescue_from UnprocessableEntityException do |e|
+      render json: { message: e.message }, status: :unprocessable_entity
+    end
   end
 
   private
