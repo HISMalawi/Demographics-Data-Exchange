a4a378baf66dcde875a9fb4d8ed28192a04fe496
Fredrick Chiyenda <fchiyenda@gmail.com>
2022-05-12 10:46:49 +0200

    Limited dashboard update for only actions that change data on the master

--- app/jobs/dashboard_socket_data_job.rb
+++ app/jobs/dashboard_socket_data_job.rb
@@ -4,12 +4,12 @@ class DashboardSocketDataJob < ApplicationJob
   def perform(*args)
     # Do something later
     ActionCable.server.broadcast('dashboard_channel', message: {
+      connected_state: DashboardService.connected_sites,
       total_new_registrations: PersonDetail.where('date_registered BETWEEN ? AND ?',Date.today.strftime('%Y-%m-%d %H:%M:%S'),Date.today.strftime('%Y-%m-%d') + ' 23:59:59').count,
       total_new_registrations_by_site: DashboardService.new_reg_by_site,
       total_new_reg_by_site_past_30: DashboardService.new_reg_past_30,
       client_movement: DashboardService.client_movements,
       npid_state: DashboardService.npids,
-      connected_state: DashboardService.connected_sites,
       site_activity: DashboardService.site_activities,
     })
   end
