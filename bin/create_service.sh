126539667896e6609f1a47c791b688000e5509e3
Senzenjani <smanjawira@gmail.com>
2022-05-19 10:12:35 +0200

    Added almost full automation to setting up dde via script

--- bin/create_service.sh
+++ bin/create_service.sh
@@ -1,14 +1,43 @@
 mode="production"
 service_name="dde4"
 ruby="2.5.3"
+production_db="dde4_production"
+username="admin"
+password="bht.dde3!"
+host="0.0.0.0"
+location="349"
+
 
 actions() {
-    tput setaf 6; read -p "Enter Application full path or press Enter to default to (/var/www/dde4): " app_dir
+    tput setaf 6; read -p "Enter DDE4 full path or press Enter to default to (/var/www/dde4): " app_dir
     if [ -z "$app_dir" ]
     then
         app_dir="/var/www/dde4"
     fi
-}
+
+    cp ${app_dir}/config/database.yml.example config/database.yml
+    cp ${app_dir}/config/secrets.yml.example  config/secrets.yml
+    cp ${app_dir}/config/storage.yml.example config/storage.yml
+
+    read -p "Site Location Id: " location
+    read -p "Production mysql username: " db_username
+    read -p "Production mysql password: " db_password
+    read -p "Sync host: " sync_host
+    read -p "Sync port: " sync_port
+    read -p "Sync username: " sync_username
+    read -p "Sync password: " sync_password
+    
+
+    sed -i -e "/^production:/,/database:/{/^\([[:space:]]*database: \).*/s//\1${production_db}/}" \
+           -e "/^production:/,/username:/{/^\([[:space:]]*username: \).*/s//\1${db_username}/}" \
+           -e "/^production:/,/password:/{/^\([[:space:]]*password: \).*/s//\1${db_password}/}" \
+           -e "/^:dde_sync_config:/,/:host:/{/^\([[:space:]]*:host: \).*/s//\1${sync_host}/}" \
+           -e "/^:dde_sync_config:/,/:port:/{/^\([[:space:]]*:port: \).*/s//\1${sync_port}/}" \
+           -e "/^:dde_sync_config:/,/:username:/{/^\([[:space:]]*:username: \).*/s//\1${sync_username}/}" \
+           -e "/^:dde_sync_config:/,/:password:/{/^\([[:space:]]*:password: \).*/s//\1${sync_password}/}" \
+            ${app_dir}/config/database.yml
+
+    }
 
 actions
 while [ ! -d $app_dir ]; do
@@ -17,9 +46,7 @@ while [ ! -d $app_dir ]; do
     actions
 done
 
-app_core=$(grep -c processor /proc/cpuinfo)
-
-read -p "Enter PORT or enter to default to (8050): " app_port
+read -p "Enter DDE PORT or press enter to default to (8050): " app_port
 
 if [ -z "$app_port" ]
 then
@@ -27,6 +54,59 @@ then
 fi
 
 
+emr_config(){
+    tput setaf 6; read -p "Enter BHT-EMR-API full path or press Enter to default to (/var/www/BHT-EMR-API): " emr_dir
+    if [ -z "$emr_dir" ]
+    then
+        emr_dir="/var/www/BHT-EMR-API"
+    fi
+
+    sed -i -e "/^dde:/,/url:/{/^\([[:space:]]*url: \).*/s//\1${host}:${app_port}/}" $emr_dir/config/application.yml 
+}
+
+emr_config
+while [ ! -d $emr_dir ]; do
+    tput setaf 1; echo "===>Directory $emr_dir DOES NOT EXISTS.<==="
+    tput setaf 7;
+    emr_config
+done
+
+declare -A program_names
+declare -A usernames
+declare -A passwords
+
+program_index=0
+add_programs(){
+    read -p "Enter EMR new program name: " program_name
+    read -p "Enter $program_name username: " program_username
+    read -p "Enter password for $program_username: " program_password
+
+    program_names[$program_index]=$program_name
+    usernames["$program_name"]=$program_username
+    passwords["$program_name"]=$program_password
+
+    read -p "Do you want to add another program(y/n): " choice
+    if [[ $choice == "y" ]]; then
+        program_index=$(($program_index+1))
+        add_programs
+    fi
+}
+
+add_programs
+for program in "${program_names[@]}"; do
+    sed -i "/dde:$/a\
+    \  ${program}:" $emr_dir/config/application.yml 
+    sed -i "/${program}:$/a\
+    \    username: ${usernames[$program]}" $emr_dir/config/application.yml 
+    sed -i "/${program}:$/a\
+    \    password: ${passwords[$program]}"  $emr_dir/config/application.yml 
+done
+
+bundle install --local
+RAILS_ENV=$mode rails db:create db:migrate db:seed
+
+
+app_core=$(grep -c processor /proc/cpuinfo)
 puma_dir=$(which puma)
 
 if [ -z "$puma_dir" ] 
@@ -125,7 +205,21 @@ echo "Cleaning up"
 rm ./${service_name}.service
 rm ./${env}.rb
 
-whenever --set "environment=${env}" --update-crontab
+login_path="$host:$app_port/v1/login?username=$username&password=$password"
+
+RESPONSE="$(sleep 5 && curl  --location --request POST $login_path)"
+
+TOKEN=`echo "$RESPONSE" | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$'`
+
+for program in "${program_names[@]}"; do
+    add_user_path="$host:$app_port/v1/add_user?username=${usernames[$program]}&password=${passwords[$program]}&location=$location"
+    RESPONSE="$(sleep 2 && curl --location --request POST ${add_user_path} --header "Authorization: ${TOKEN}")"
+done
+
+add_user_path="$host:$app_port/v1/add_user?username=${sync_username}&password=${sync_password}&location=$location"
+RESPONSE="$(sleep 2 && curl --location --request POST ${add_user_path} --header "Authorization: ${TOKEN}")"
+
+echo "Users created"
 
 echo "Sync cron job configured"
  
@@ -146,8 +240,12 @@ echo "Restart Service"
 echo "sudo service ${service_name} restart "
 echo "Stop Service"
 echo "sudo service ${service_name} stop"
+echo "Disable service"
+echo "sudo systemctl disable ${service_name}"
 echo "---------------------------"
 echo "Thank You!"
+sudo service ${service_name} status
+
 
 
 
