bb60fbbd5968d5a16a2fd21ccb0fed47c7b77651
Fredrick Chiyenda <fchiyenda@gmail.com>
2022-08-11 16:50:02 +0200

    Add json column if mysql version is >= 5.7

--- db/migrate/20220628123446_create_dashboard_stats.rb
+++ db/migrate/20220628123446_create_dashboard_stats.rb
@@ -1,10 +1,13 @@
 class CreateDashboardStats < ActiveRecord::Migration[5.2]
-  def change
-    create_table :dashboard_stats, id: false, :primary_key => :name do |t|
-      t.string :name, null: false
-      t.json :value, null: false
+  version = `mysql --version`.chomp
+  if (version[(version.index(',') - 6),3].to_f >= 5.7)
+    def change
+      create_table :dashboard_stats, id: false, :primary_key => :name do |t|
+        t.string :name, null: false
+        t.json :value, null: false
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
