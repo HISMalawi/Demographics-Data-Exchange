60e1fe8740ebbf3f8930be8cd2c7c67c7ad2c650
Roy Chanunkha <roychanunkha@gmail.com>
2022-08-11 17:21:52 +0200

    changing from chomp to using the mysql variables

--- db/migrate/20220628123446_create_dashboard_stats.rb
+++ db/migrate/20220628123446_create_dashboard_stats.rb
@@ -1,8 +1,9 @@
 class CreateDashboardStats < ActiveRecord::Migration[5.2]
-  version = `mysql --version`.chomp
-  if (version[(version.index(',') - 6),3].to_f >= 5.7)
-    def change
-      create_table :dashboard_stats, id: false, :primary_key => :name do |t|
+  # version = `mysql --version`.chomp
+  def change
+    result = ActiveRecord::Base.connection.select_one "SHOW VARIABLES WHERE variable_name = 'version'"
+    unless result['Value'].include?('5.6')
+      create_table :dashboard_stats, id: false, primary_key: :name do |t|
         t.string :name, null: false
         t.json :value, null: false
 
