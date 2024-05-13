e4cd0a56b4180e19508003237856e3a7f4b4c592
Senzenjani <smanjawira@gmail.com>
2024-05-13 10:16:56 +0200

    Choosing between rbenv and  rvm to use

--- bin/dde4_update_script.sh
+++ bin/dde4_update_script.sh
@@ -6,6 +6,11 @@ read -p "Enter DDE port please : " port_number
 # Prompt user for the username
 read -p "Enter server username (e.g., meduser): " username
 
+# Prompt user to select ruby version manager to use
+read -sn1 -s -p "SELECT Ruby Version Manager to use
+(1)Rbenv
+(2)rvm" ruby_version_manager
+
 # Then Check if the service is running on port 8050
 if lsof -Pi :$port_number -sTCP:LISTEN -t >/dev/null ; then
 # If the service is running, stop it
@@ -19,8 +24,8 @@ fi
 echo "Killing the process using port 8050..."
 sudo fuser -k 8050/tcp
 
-# Remove the vendor folder and Gemfile.loc
-echo "Removing vendor folder and Gemfile.loc"
+# Remove the vendor folder and Gemfile.lock
+echo "Removing vendor folder and Gemfile.lock"
 sudo rm -rf vendor/
 sudo rm -f Gemfile.lock
 
@@ -28,9 +33,23 @@ sudo rm -f Gemfile.lock
 echo "RUnning Bundle install ..."
 bundle install
 
-#Get the path of Puma and Ruby
-puma_path="$(sudo -u $username bash -lc 'which puma')"
-ruby_path="$(sudo -u $username bash -lc 'which ruby')"
+#Get the path of Puma, Ruby and ruby version manager
+puma_path="$( bash -lc 'which puma')"
+ruby_path="$( bash -lc 'which ruby')"
+
+if [ -z "$puma_path"]; then
+    puma_path="puma"
+fi
+
+
+if [[ $ruby_version_manager  == 1 ]]; then
+   version_manager_path="$( bash -lc 'which rbenv')"
+   bundle_path="$(bash -lc 'which bundle')"
+   new_exec_start="'$version_manager_path local 3.2.0 && $bundle_path exec $puma_path -C /var/www/dde4/config/server/production.rb'"
+else
+   new_exec_start="/bin/bash -lc 'rvm  use 3.2.0 && bundle exec $puma_path -C /var/www/dde4/config/server/production.rb'"
+fi
+
 
 
 # Edit the ExecStart directive in the dde4.service file
@@ -40,12 +59,6 @@ echo "Changing to Ruby 3.2.0"
 #'/home/ubuntu/.rbenv/bin/rbenv local 3.2.0 && /home/ubuntu/.rbenv/shims/bundle exec puma -C /var/www/dde4/config/server/production.rb'
 
 
-if [ -z "$puma_path"]; then
-    puma_path="puma"
-fi
-
-
-new_exec_start="/bin/bash -lc 'rvm use 3.2.0 && bundle exec $puma_path -C /var/www/dde4/config/server/production.rb'"
 
 # Commenting out all occurrences of ExecStart lines
 echo "Commenting out all occurrences of ExecStart lines in dde4.service..."
--- vendor/bundle/ruby/2.5.0/gems/activerecord-import-1.3.0/lib/activerecord-import/version.rb
+++
@@ -1,5 +0,0 @@
-module ActiveRecord
-  module Import
-    VERSION = "1.3.0".freeze
-  end
-end
