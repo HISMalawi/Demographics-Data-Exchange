6d28557963176055e0b652f148f5e9b6d77f2e7f
Senzenjani <smanjawira@gmail.com>
2024-10-10 14:43:08 +0200

    Configure Sidekiq to use separate Redis databases for app segmentation

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,29 @@
 # config/initializers/sidekiq.rb
+require 'redis'
+
+redis = Redis.new(url: 'redis://localhost:6379/0') # Start with db 0
+
+def find_free_redis_db(redis_client, max_db = 15)
+  (0..max_db).each do |db|
+    redis_client.select(db)
+    size = redis_client.dbsize
+    return db if size == 0
+  end
+  raise "No free Redis databases available."
+end
+
+free_db = find_free_redis_db(redis)
 
 Sidekiq.configure_server do |config|
     config.on(:startup) do
       Sidekiq::Scheduler.reload_schedule!
     end
+
+    config.redis = { db: free_db, url: "redis://localhost:6379/#{free_db}" }
+end
+
+Sidekiq.configure_client do |config|
+    config.redis = { db: free_db, url: "redis://localhost:6379/#{free_db}" }
 end
   
 Rails.application.configure do
