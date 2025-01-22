0393eaf3d3dada48cd20fce3b71edf7eddfec023
Senzenjani <smanjawira@gmail.com>
2025-01-22 10:34:28 +0200

    Added national_id validation

--- app/models/person_detail.rb
+++ app/models/person_detail.rb
@@ -4,4 +4,5 @@ class PersonDetail < ApplicationRecord
   has_many :person_details_audit, foreign_key: :person_uuid, primary_key: :person_uuid
   validates :creator,:location_created_at,:person_uuid,:date_registered,:last_edited,:first_name,:last_name, presence: true
   validates :person_uuid, :npid, uniqueness: true
+  validates :national_id, uniqueness: true, allow_blank: true
 end
