35938c691ea88b7a681d0091f4275f316945709b
Senzenjani <smanjawira@gmail.com>
2025-01-22 17:10:50 +0200

    Improved national id validation

--- app/models/person_detail.rb
+++ app/models/person_detail.rb
@@ -5,4 +5,15 @@ class PersonDetail < ApplicationRecord
   validates :creator,:location_created_at,:person_uuid,:date_registered,:last_edited,:first_name,:last_name, presence: true
   validates :person_uuid, :npid, uniqueness: true
   validates :national_id, uniqueness: true, allow_blank: true
+  validate :valid_national_id, if: -> { national_id.present? }
+
+  private
+
+  # Custom validation for Malawi National ID
+  def valid_national_id
+    unless national_id.match?(/\A(?!.*S)[A-Z0-9]{8}\z/)
+      errors.add(:national_id, "must be exactly 8 alphanumeric characters and cannot contain the letter 'S'")
+    end
+  end
 end
+ 
