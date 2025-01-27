a9c0a7cafb7aab9248254c4441795a2653ba1f18
Senzenjani <smanjawira@gmail.com>
2025-01-27 10:04:56 +0200

    Removed unique constraint to national id on personal detail audit table

--- db/migrate/20250123091551_add_unique_constraint_to_national_id.rb
+++ db/migrate/20250123091551_add_unique_constraint_to_national_id.rb
@@ -1,6 +1,5 @@
 class AddUniqueConstraintToNationalId < ActiveRecord::Migration[7.0]
   def change
     add_index :person_details, :national_id, unique: true
-    add_index :person_details_audits, :national_id, unique: true
   end
 end
