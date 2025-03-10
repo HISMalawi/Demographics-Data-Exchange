4a3e2e76d695242981dfa032185fd7861579e731
Senzenjani <smanjawira@gmail.com>
2025-03-10 11:28:55 +0200

    Fixed calling of dashboard service and delivering of email

--- app/jobs/last_seen_last_synced_job.rb
+++ app/jobs/last_seen_last_synced_job.rb
@@ -3,19 +3,20 @@
 # This is a class that checks for email nofication
 class LastSeenLastSyncedJob < ApplicationJob
   require 'dashboard_service'
-  queue_as :default
+  queue_as :last_seen_last_synced
 
   def perform(*args)
     # Do something later
 
-    sites = DashBoardService.site_activities
+    sites = DashboardService.site_activities
 
     sites.each do |site|
       # Check if last seen it greater than 3 days
       if site['days_since_last_activity'].to_i > 3
-        LastSyncedMailer.last_synced_more_than_3_days(site)
+        LastSyncedMailer.last_synced_more_than_3_days(site).deliver_later
+        break
       elsif site['days_since_last_seen'].to_i > 3
-        LastSeenMailer.last_seen_more_than_3_days(site)
+        LastSeenMailer.last_seen_more_than_3_days(site).deliver_later
       end
     end
   end
