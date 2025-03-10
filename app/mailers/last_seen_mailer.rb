81c0251a8772baf077df7b9693dc3cf7fb7d3b4a
Senzenjani <smanjawira@gmail.com>
2025-03-10 14:00:52 +0200

    Fixed incorrect reference to site name and location attributes

--- app/mailers/last_seen_mailer.rb
+++ app/mailers/last_seen_mailer.rb
@@ -6,7 +6,7 @@ class LastSeenMailer < ApplicationMailer
 
   def last_seen_more_than_3_days(site_details)
     mailing_list = MailingList
-                   .joins(:mailer_locations).where('mailer_locations.location_id = ?', site_details[:location_id])
+                   .joins(:mailer_locations).where('mailer_locations.location_id = ?', site_details["location_id"])
                    .pluck(:email)
 
     mailing_list ||= MailingList
@@ -14,16 +14,16 @@ class LastSeenMailer < ApplicationMailer
                      .where('roles.role = ?', 'Admin')
                      .pluck(:email)
 
-    return unless mail_not_sent
+    #return unless mail_not_sent
 
-    mail(to: mailing_list,subject: "Please check #{site_details[:site_name]} was last seen more than 3 days ago")
+    mail(to: mailing_list,subject: "Please check #{site_details["site_name"]} was last seen more than 3 days ago")
   end
 
   private
 
   def mail_not_sent
-    MailingLog.where('location_id = ? AND notification_type = ? \
-            AND data(created_at) = ?', site_details[:location_id], \
+    MailingLog.where('location_id = ? AND notification_type = ? 
+            AND data(created_at) = ?', @site_details[:location_id], 
                      "#{mail.to} #{mail.subject}", Date.today).blank?
   end
 
