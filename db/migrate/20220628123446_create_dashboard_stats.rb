dcbf9a941ec8ad6bf76d59bde4cff5a8ec2edc51
fchiyenda <fchiyenda>
2025-03-12 12:43:32 +0200

    Fixed dashboard stats migration bug

--- db/migrate/20220628123446_create_dashboard_stats.rb
+++ db/migrate/20220628123446_create_dashboard_stats.rb
@@ -3,7 +3,7 @@ class CreateDashboardStats < ActiveRecord::Migration[5.2]
   def change
     result = ActiveRecord::Base.connection.select_one "SHOW VARIABLES WHERE variable_name = 'version'"
     table_exist = ActiveRecord::Base.connection.select_one("Show tables like '%dashboard_stats%';")
-    return unless !result['Value'].include?('5.6') && table_exist.values.blank?
+    return unless !result['Value'].include?('5.6') && table_exist
 
     create_table :dashboard_stats, id: false, primary_key: :name do |t|
       t.string :name, null: false
