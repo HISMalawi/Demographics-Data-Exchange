a0f496ecdad9f49d072fd6d92f5febee026090fb
Senzenjani <smanjawira@gmail.com>
2025-03-20 15:12:12 +0200

    Log district mailed to

--- app/mailers/low_npid_notification_mailer.rb
+++ app/mailers/low_npid_notification_mailer.rb
@@ -2,35 +2,36 @@ class LowNpidNotificationMailer < ApplicationMailer
     after_action :log_mail
 
     def low_npid(district_data)
-        @district_data = district_data
+      @district_data = district_data
         
-        @mailing_list = MailingList.joins(:mailer_districts).where(
+      @mailing_list = MailingList.joins(:mailer_districts).where(
             'mailer_districts.district_id = ?', 
             district_data[:district_id]).pluck(:email)
+
+      if @mailing_list.blank?
+        @mailing_list = MailingList.joins(:roles).where('roles.role = ?', 'Admin').pluck(:email)
+      end
         
-        if @mailing_list.blank?            
-            @mailing_list = MailingList.joins(:roles).where('roles.role = ?', 'Admin').pluck(:email) 
-            mail(to: @mailing_list,
-             subject: "Low NPIDs in #{district_data[:name]}")
-        else
-            mail(to: @mailing_list,
-            # cc: MailingList.joins(:roles).where('roles.role = ?', 'Admin').pluck(:email),
-            subject: "Low NPIDs in #{district_data[:name]}") 
-        end
+      if mail_not_sent
+        mail(to: @mailing_list, subject: "NPIDs running out in  #{district_data[:name]}")
+      end
     end
 
     private
 
     def mail_not_sent
-       # MailingLog.where('location_id = ? AND notification_type = ?  
-       #       AND created_at = ?', @site_details[:location_id],  
-        #      "#{mail.to} #{mail.subject}", Date.today).blank?
+      MailingLog.where(
+        district_id: @district_data[:district_id],
+        notification_type:  "#{mail.to} #{mail.subject}",
+        created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day )
     end
-
+  
     def log_mail
-        #MailingLog.create!(location_id: @site_details[:location_id],
-        #                  notification_type: "#{mail.to} #{mail.subject}"
-        #                )
-        #Rails.logger.info("Email sent #{mail.to} - #{mail.subject}")
+      return unless mail.perform_deliveries
+  
+      MailingLog.create!(
+        district_id: @district_data[:district_id],
+        notification_type: "#{mail.to} #{mail.subject}"
+      )
     end
 end
