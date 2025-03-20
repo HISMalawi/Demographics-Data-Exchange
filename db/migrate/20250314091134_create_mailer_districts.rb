24aae30d485d9d887bbf1aa215d44054f61710d6
Senzenjani <smanjawira@gmail.com>
2025-03-20 15:11:50 +0200

    Add district to mailer log

--- db/migrate/20250314091134_create_mailer_districts.rb
+++ db/migrate/20250314091134_create_mailer_districts.rb
@@ -8,8 +8,10 @@ class CreateMailerDistricts < ActiveRecord::Migration[7.0]
         t.timestamps
       end
 
+      add_column  :mailing_logs, :district_id, :integer
       add_foreign_key :mailer_districts, :districts, column: :district_id, primary_key: :district_id
       add_foreign_key :mailer_districts, :mailing_lists, column: :mailer_id, primary_key: :id
+      add_foreign_key :mailing_logs, :districts, column: :district_id, primary_key: :district_id
     else
       puts 'Only applicable on master'
     end
