8a43cd3bd64179eea8533a43bd6dab34bae6ee7f
fchiyenda <fchiyenda>
2025-03-27 08:50:12 +0200

    Removed scope bug for location NPID for reconcilation

--- app/jobs/location_npid_job.rb
+++ app/jobs/location_npid_job.rb
@@ -3,8 +3,8 @@ class LocationNpidJob < ApplicationJob
 
   def perform(*args)
     # Do something later
-    location_npid_balance = LocationNpid.all.group(:assigned).count
-    dashboard_stat = DashboardStat.find_or_create_by(name: "location_npid_balance", value: {}) do |stat|
+    location_npid_balance = LocationNpid.unscoped.group(:assigned).count
+    dashboard_stat = DashboardStat.find_or_create_by(name: 'location_npid_balance', value: {}) do |stat|
       stat.value = {}
     end
     dashboard_stat.update(value: location_npid_balance)
