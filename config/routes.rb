2f069d1cccb8466b65d1f99424aeb1ec76223d7d
Senzenjani <smanjawira@gmail.com>
2025-06-16 14:16:18 +0200

    Moved require user management to application.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Sidekiq::Web.use ActionDispatch::Cookies
 Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
 
 Rails.application.routes.draw do
+  mount UserManagement::Engine, at: '/v1' 
   resources :mailing_lists
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   mount Sidekiq::Web => '/v1/sidekiq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
