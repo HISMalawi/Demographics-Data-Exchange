6cf15dcab0561edc9a074057b1e253d4d79ec353
Senzenjani <smanjawira@gmail.com>
2025-07-11 11:18:50 +0200

    Deactivating mailing lists than voiding

--- app/controllers/api/v1/mailing_lists_controller.rb
+++ app/controllers/api/v1/mailing_lists_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::MailingListsController < ApplicationController
 
   # GET v1/mailing_lists
   def index
-    @mailing_lists = MailingList.all
+    @mailing_lists = MailingList.joins(:roles)
+                                .select("mailing_lists.*, roles.role")
 
     render json: @mailing_lists
   end
@@ -38,7 +39,7 @@ class Api::V1::MailingListsController < ApplicationController
 
   # DELETE /mailing_lists/1
   def destroy
-    @mailing_list.update(voided: true)
+    @mailing_list.update(deactivated: true)
   end
 
   def roles
