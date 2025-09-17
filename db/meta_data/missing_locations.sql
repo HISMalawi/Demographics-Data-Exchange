19470408b6c133e9576330fc7d40a1b660cc41f7
fchiyenda <fchiyenda>
2025-09-17 14:04:42 +0200

    Removed replacing of locations

--- db/meta_data/missing_locations.sql
+++ db/meta_data/missing_locations.sql
@@ -1,108 +1,56 @@
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1079, 'Mangochi Pakachere DIC', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1080, 'Monkey Bay Pakachere DIC', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1081, 'Matawale DIC', 28, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1082, 'Likuni DIC', 11, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1083, 'Area 23_DIC', 11, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1084, 'Area 25 DIC', 11, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1085, 'Area 36 DIC', 11, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1086, 'Chirimba DIC', 2, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1087, 'Naperi DIC', 2, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1088, 'Bangwe DIC', 2, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1089, 'Ekwendeni DIC', 17, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1090, 'Katoto DIC', 17, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1091, 'Zolozolo DIC', 17, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1092, 'Area 47 DIC', 11, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1093, 'Chitimba DIC', 25, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1094, 'Machinga DIC', 12, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1095, 'Ntaja DIC', 12, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1096, 'Blantyre DIC', 2, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1097, 'Nyambadwe DIC', 2, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1098, 'Mangochi DIC', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1099, 'Mangochi FPAM', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1100, 'Phalombe District Hospital', 24, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1101, 'Charity Private Clinic', 28, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1102, 'Hope Medi Clinic', 28, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1103, 'Mbanila Health Centre', 12, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1105, 'Mzuzu DIC', 17, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1106, 'Mangochi Medi Clinic', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1107, 'Kwidu Community Clinic', 19, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1108, 'Mumbwe Private Clinic', 17, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1109, 'Maganga Dispensary', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1110, 'Zomba Airwing Clinic', 28, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1111, 'Nchalo Hunger', 3, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1112, 'Osiyana Health Centre', 21, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1113, 'College of Medicine Pvt Clinic', 2, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1114, 'Our Lady of Mount Carmel Community Hospital', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1115, 'Malavi Health Centre', 4, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1116, 'Kazyozyo Health Centre', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1117, 'Welliness Centre', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1118, 'Chimwankango', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1119, 'Mphelero Health Centre', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1120, 'BLM Mchinji', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1121, 'Chikowa Health Center', 22, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1122, 'Phanga Dispensary', 22, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1123, 'Biwi Health Center', 11, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1124, 'Chombe dispensary Nkhatabay', 19, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1125, 'Engalaweni Dispensary Mzimba South', 17, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1126, 'Chambwe Health Center (Kasungu)', 9, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1127, 'Anchor Farm (Kasungu)', 9, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1128, 'Mwima Health Centre', 1, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1129, 'St Marys Clinic', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1130, 'Masasa Dispensary', 13, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1131, 'Charity Private Clinic', 28, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1132, 'Fanuel dispensary', 14, 1, NOW(), NOW());
-      INSERT INTO locations ( location_id, name, district_id, creator, created_at, updated_at)
-      VALUES (1133, 'Namfuse Health Center', 14, 1, NOW(), NOW());
+      INSERT IGNORE INTO locations (location_id, name, district_id, creator, created_at, updated_at)
+      VALUES
+            (1079, 'Mangochi Pakachere DIC', 13, 1, NOW(), NOW()),
+            (1080, 'Monkey Bay Pakachere DIC', 13, 1, NOW(), NOW()),
+            (1081, 'Matawale DIC', 28, 1, NOW(), NOW()),
+            (1082, 'Likuni DIC', 11, 1, NOW(), NOW()),
+            (1083, 'Area 23_DIC', 11, 1, NOW(), NOW()),
+            (1084, 'Area 25 DIC', 11, 1, NOW(), NOW()),
+            (1085, 'Area 36 DIC', 11, 1, NOW(), NOW()),
+            (1086, 'Chirimba DIC', 2, 1, NOW(), NOW()),
+            (1087, 'Naperi DIC', 2, 1, NOW(), NOW()),
+            (1088, 'Bangwe DIC', 2, 1, NOW(), NOW()),
+            (1089, 'Ekwendeni DIC', 17, 1, NOW(), NOW()),
+            (1090, 'Katoto DIC', 17, 1, NOW(), NOW()),
+            (1091, 'Zolozolo DIC', 17, 1, NOW(), NOW()),
+            (1092, 'Area 47 DIC', 11, 1, NOW(), NOW()),
+            (1093, 'Chitimba DIC', 25, 1, NOW(), NOW()),
+            (1094, 'Machinga DIC', 12, 1, NOW(), NOW()),
+            (1095, 'Ntaja DIC', 12, 1, NOW(), NOW()),
+            (1096, 'Blantyre DIC', 2, 1, NOW(), NOW()),
+            (1097, 'Nyambadwe DIC', 2, 1, NOW(), NOW()),
+            (1098, 'Mangochi DIC', 13, 1, NOW(), NOW()),
+            (1099, 'Mangochi FPAM', 13, 1, NOW(), NOW()),
+            (1100, 'Phalombe District Hospital', 24, 1, NOW(), NOW()),
+            (1101, 'Charity Private Clinic', 28, 1, NOW(), NOW()),
+            (1102, 'Hope Medi Clinic', 28, 1, NOW(), NOW()),
+            (1103, 'Mbanila Health Centre', 12, 1, NOW(), NOW()),
+            (1105, 'Mzuzu DIC', 17, 1, NOW(), NOW()),
+            (1106, 'Mangochi Medi Clinic', 13, 1, NOW(), NOW()),
+            (1107, 'Kwidu Community Clinic', 19, 1, NOW(), NOW()),
+            (1108, 'Mumbwe Private Clinic', 17, 1, NOW(), NOW()),
+            (1109, 'Maganga Dispensary', 13, 1, NOW(), NOW()),
+            (1110, 'Zomba Airwing Clinic', 28, 1, NOW(), NOW()),
+            (1111, 'Nchalo Hunger', 3, 1, NOW(), NOW()),
+            (1112, 'Osiyana Health Centre', 21, 1, NOW(), NOW()),
+            (1113, 'College of Medicine Pvt Clinic', 2, 1, NOW(), NOW()),
+            (1114, 'Our Lady of Mount Carmel Community Hospital', 14, 1, NOW(), NOW()),
+            (1115, 'Malavi Health Centre', 4, 1, NOW(), NOW()),
+            (1116, 'Kazyozyo Health Centre', 14, 1, NOW(), NOW()),
+            (1117, 'Welliness Centre', 14, 1, NOW(), NOW()),
+            (1118, 'Chimwankango', 14, 1, NOW(), NOW()),
+            (1119, 'Mphelero Health Centre', 14, 1, NOW(), NOW()),
+            (1120, 'BLM Mchinji', 14, 1, NOW(), NOW()),
+            (1121, 'Chikowa Health Center', 22, 1, NOW(), NOW()),
+            (1122, 'Phanga Dispensary', 22, 1, NOW(), NOW()),
+            (1123, 'Biwi Health Center', 11, 1, NOW(), NOW()),
+            (1124, 'Chombe dispensary Nkhatabay', 19, 1, NOW(), NOW()),
+            (1125, 'Engalaweni Dispensary Mzimba South', 17, 1, NOW(), NOW()),
+            (1126, 'Chambwe Health Center (Kasungu)', 9, 1, NOW(), NOW()),
+            (1127, 'Anchor Farm (Kasungu)', 9, 1, NOW(), NOW()),
+            (1128, 'Mwima Health Centre', 1, 1, NOW(), NOW()),
+            (1129, 'St Marys Clinic', 13, 1, NOW(), NOW()),
+            (1130, 'Masasa Dispensary', 13, 1, NOW(), NOW()),
+            (1131, 'Charity Private Clinic', 28, 1, NOW(), NOW()),
+            (1132, 'Fanuel dispensary', 14, 1, NOW(), NOW()),
+            (1133, 'Namfuse Health Center', 14, 1, NOW(), NOW());
