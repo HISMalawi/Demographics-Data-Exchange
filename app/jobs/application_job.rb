572f5877111b41b36f08a69ba3773f5649ca74f0
fchiyenda <fchiyenda>
2025-10-06 14:38:12 +0200

    Add ttl for job lock Sometimes job locks are hanging so added a ttl so that acts as a fail safe.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,4 @@
 class ApplicationJob < ActiveJob::Base
-  unique :until_executed, on_conflict: :log
+  unique :until_executed, lock_ttl: 1.hour, on_conflict: :log
   sidekiq_options retry: false
 end
