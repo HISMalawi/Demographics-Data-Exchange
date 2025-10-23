d73da0506f8c73ea6ef61ab37c9ba38c9330488a
Senzenjani <smanjawira@gmail.com>
2025-10-23 15:16:33 +0200

    Localised tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.{html,erb,html.erb}',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
   ],
   theme: {
     extend: {},
