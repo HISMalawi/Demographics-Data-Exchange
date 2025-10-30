e4531be5227724ae47473c42f07ab507c3cca357
Senzenjani <smanjawira@gmail.com>
2025-10-30 09:55:59 +0200

    Precompiled assets

---
+++ public/assets/application-1d0c293b47efdb6247870dd3195f597e50c5bafa0b783a5abb763bc31ebc6850.js
@@ -0,0 +1,20 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+import PageController from "./controllers/page_controller"
+ 
+import { autoRunTroubleshooting, fetchStats, runTestSync } from "../troubleshooting" 
+
+const application = Application.start()
+application.register("page", PageController)
+
+
+document.getElementById("btn-test-sync").addEventListener("click", function () {
+  // your code here
+  runTestSync();
+});
+
+document.getElementById("rerun-diagnostics").addEventListener("click", function () {
+  // your code here
+  autoRunTroubleshooting();
+});
