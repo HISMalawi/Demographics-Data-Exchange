7af0ee7ec5cf3e2c8e9f3e31fe69a7e03b722d76
Senzenjani <smanjawira@gmail.com>
2025-11-04 14:22:16 +0200

    Removed finding of location_id using ip_address

--- app/helpers/network_helper.rb
+++ app/helpers/network_helper.rb
@@ -6,25 +6,4 @@ module NetworkHelper
     @current_private_ip ||=  Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
   end
 
-  # Returns all locations with a matching IP
-  def locations_by_current_ip
-    locations_by_current_ip ||= Location.where(ip_address: current_private_ip)
-  end
-
-  # Checks for conflicts or returns the matching location (or nil if none)
-  def check_ip_conflict!
-    return @current_location if defined?(@current_location)
-
-    ip_address = current_private_ip
-    matching_locations = locations_by_current_ip
-    
-    if matching_locations.count > 1
-      raise "<strong>Conflict Detected:</strong> The IP address <strong>#{ip_address}</strong> 
-      is linked to <strong>multiple locations</strong>. Please verify your network configuration or update the location records."
-    elsif matching_locations.exists?
-      @current_location = matching_locations.first
-    else
-      nil
-    end
-  end
 end
