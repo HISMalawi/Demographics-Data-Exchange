b074d6d19fcdd8118dab759396e3ccc7054f98f4
Senzenjani <smanjawira@gmail.com>
2025-11-05 10:46:54 +0200

    Create dashboard caches only on MASTER

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,14 +32,18 @@ Config.create!(config: 'npid_seq',
                description: 'NPID pull vector clock',
                uuid: 'ebc28cab-b7d8-11eb-8cf6-dc41a91e235e') unless Config.find_by_config('npid_seq')
 
-DashboardStat.find_or_create_by(name: 'npid_balance',
+if  ENV['MASTER'] == 'true' {
+  DashboardStat.find_or_create_by(name: 'npid_balance',
                                 value: {})
 
-DashboardStat.find_or_create_by(name: 'location_npid_balance',
-                                value: {})
+  DashboardStat.find_or_create_by(name: 'location_npid_balance',
+                                  value: {})
 
-DashboardStat.find_or_create_by(name: 'dashboard_stats',
-                                value: {})
+  DashboardStat.find_or_create_by(name: 'dashboard_stats',
+                                  value: {})
+}               
+
+
 
 unless User.exists?
 #Load proxy Meta data
