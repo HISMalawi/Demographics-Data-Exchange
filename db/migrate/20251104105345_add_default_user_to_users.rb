1a69b4741583185d0e6d93727d3c8ae691d7112b
fchiyenda <fchiyenda>
2025-12-09 11:25:36 +0200

    Add Migration to update default user

--- db/migrate/20251104105345_add_default_user_to_users.rb
+++ db/migrate/20251104105345_add_default_user_to_users.rb
@@ -1,5 +1,13 @@
 class AddDefaultUserToUsers < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :users, :default_user, :boolean, default: false, null: false
+
+    # Set the admin user as default user
+    User.reset_column_information
+    User.where(username: 'admin').update_all(default_user: true)
+  end
+
+  def down
+    remove_column :users, :default_user
   end
 end
