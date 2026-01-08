9dccbbb83975edc0e651a6bbfb4af64f446ec8ac
fchiyenda <fchiyenda>
2026-01-08 14:51:00 +0200

    Stop tracking public/assets

--- config/puma.rb
+++ config/puma.rb
@@ -4,16 +4,17 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch('PORT') { 8050 }
+port ENV.fetch("PORT") {  8050  }
+
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch('RAILS_ENV') { 'production' }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -22,7 +23,7 @@ environment ENV.fetch('RAILS_ENV') { 'production' }
 # processes).
 #
 
-workers ENV.fetch('WEB_CONCURRENCY') { 12 / 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 12/2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
